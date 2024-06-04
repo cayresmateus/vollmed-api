fe4a96dea5a41103c448cda167a3811fe552f192
mateus cayres <mateustfcayres@gmail.com>
2024-06-04 15:21:02 -0400

    feat: not showing canceled consultas

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -2,5 +2,11 @@ package med.voll.api.domain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    Boolean existsByMedicoIdAndDataAndMotivoCancelamentoIsNull(Long idMedico, LocalDateTime data);
+
+
+    Boolean existsByPacienteIdAndDataBetween(Long pacienteId, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 }
